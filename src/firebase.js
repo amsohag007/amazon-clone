feb685f7161b8377fe15d7a1a41f7dfe871570c4
amsohag007 <amsohag007@gmail.com>
2020-09-24 23:22:13 +0600

    basic setup, header+banner stylng

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCx7M5_0dP-xZIRyc8KhqzkfkKTDgixlAk",
+    authDomain: "clone-fire-stripe.firebaseapp.com",
+    databaseURL: "https://clone-fire-stripe.firebaseio.com",
+    projectId: "clone-fire-stripe",
+    storageBucket: "clone-fire-stripe.appspot.com",
+    messagingSenderId: "695989445226",
+    appId: "1:695989445226:web:a0b4a7c022a69aaaae2013"
+  };
