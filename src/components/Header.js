764da21dd0b3c3eec5ed1c1aeebd0d7033beebc0
amsohag007 <amsohag007@gmail.com>
2020-09-26 00:46:01 +0600

    build checkout and subtotal component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,19 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header__logo"
-        alt="logo"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-      />
+      <Link to="/">
+        <img
+          className="header__logo"
+          alt="logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      </Link>
+
       <div className="header__search">
         <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon" />
@@ -32,15 +36,18 @@ function Header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineOne">Prime</span>
         </div>
-        <div className="header__optionBasket">
-          <ShoppingBasketIcon />
-          <span
-            className="header__optionLineTwo
+
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span
+              className="header__optionLineTwo
            header__basketCount"
-          >
-            0
-          </span>
-        </div>
+            >
+              0
+            </span>
+          </div>
+        </Link>
       </div>
     </div>
   );
