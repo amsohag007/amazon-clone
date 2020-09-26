231264d25b51add0c5b08fd034e4cb38df6d1205
amsohag007 <amsohag007@gmail.com>
2020-09-26 16:23:13 +0600

    update subtotal and remove action functionality,  style CheckoutProduct, Complete checkout screen with reducer and selector context api

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { useStateValue } from "../StateProvider";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -13,6 +17,16 @@ function Checkout() {
         />
         <div>
           <h2 className="checkout__tittle">Your Shopping Busket</h2>
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+
           {/* BasketItem */}
           {/* BasketItem */}
           {/* BasketItem */}
